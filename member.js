70a8b812793682e69357f444c0f1099425bd91a8
Dennis Kojo Ayertey <73997063+analystden@users.noreply.github.com>
2024-06-27 23:45:51 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,8 @@
+function skillsMember() {
+    var skills = {
+        name: 'John',
+        age: 20,
+        skills: ['JavaScript', 'React', 'Node', 'Python', 'Django']
+    };
+    return skills;
+}
